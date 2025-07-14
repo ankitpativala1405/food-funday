fc6db6f5cae0b79ebb670662afe989a6c7013d92
Ridham1409 <ridhambhavnagariya@gmail.com>
2025-07-14 17:02:36 +0530

    update||14-07

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import DbConnect from "./config/DbConnect.js";
 import TableRouter from "./router/Table.router.js";
+import cors from 'cors'
 
 dotenv.config();
 
 const port = process.env.PORT;
 const app = express();
+app.use(cors())
 app.use(TableRouter);
 
 app.listen(port, () => {
